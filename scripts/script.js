5f88d2eb84535bdeea15861ae260c852a1459d64
Alikhan Tynyshbayev <atynyshbayevv@yandex.kz>
2020-10-05 23:02:42 +0600

    corrected ver

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,38 @@
-let formElement = document.querySelector('.popup__form');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__text');
-let closeButton = document.querySelector('.popup__close-button');
-let popupProfile =document.querySelector('.popup');
-let openButtonPopupAdd = document.querySelector('.popup__save-button');
-let openButtonPopup = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector(".popup__form");
+let nameProfile = document.querySelector(".profile__name");
+let jobProfile = document.querySelector(".profile__text");
+let closeButton = document.querySelector(".popup__close-button");
+let popupProfile = document.querySelector(".popup");
+let openButtonPopupAdd = document.querySelector(".popup__save-button");
+let openButtonPopup = document.querySelector(".profile__edit-button");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    let nameInput = document.querySelector('.popup__form-name');
-    let jobInput = document.querySelector('.popup__form-text');
-    const nameInputValue = nameInput.value;
-    const jobInputValue = jobInput.value;
-    nameProfile.textContent = nameInputValue;
-    jobProfile.textContent = jobInputValue;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector(".popup__form-name");
+  let jobInput = document.querySelector(".popup__form-text");
+  const nameInputValue = nameInput.value;
+  const jobInputValue = jobInput.value;
+  nameProfile.textContent = nameInputValue;
+  jobProfile.textContent = jobInputValue;
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
 function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-  }
+  popup.classList.remove("popup_opened");
+}
 
-  function openPopup(popup) {
-    popup.classList.add('popup_opened');
-  }
-  
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
 function openProfilePopup() {
+  openPopup(popupProfile);
+}
+openButtonPopup.addEventListener("click", openProfilePopup);
 
-    openPopup(popupProfile);
-  }
-  openButtonPopup.addEventListener('click', openProfilePopup);
-
-
-
-
-closeButton.addEventListener('click', closePopup);
-closeButton.addEventListener('click', function () {
-    closePopup(popupProfile);
-  });
-  
+closeButton.addEventListener("click", closePopup);
+closeButton.addEventListener("click", function () {
+  closePopup(popupProfile);
+});
+formElement.addEventListener("submit", formSubmitHandler);
