b952b37b03c57b248a35464248da57a1face1599
Alikhan Tynyshbayev <atynyshbayevv@yandex.kz>
2020-10-21 16:39:45 +0600

    secondtry2

--- scripts/script.js
+++ scripts/script.js
@@ -69,6 +69,15 @@ function openPopup() {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
+function closePopup() {
+  popupProfile.classList.remove("popup_opened");
+}
+function handleOpenPopup(popUpAdd) {
+  popUpAdd.classList.add("popup_opened");
+}
+function handleClosePopup(popUpAdd) {
+  popUpAdd.classList.remove("popup_opened");
+}
 const handleLikePressed = (event) => {
   event.target
     .closest(".elements__like")
@@ -98,15 +107,6 @@ const getItems = (data) => {
   return card;
 };
 
-function closePopup() {
-  popupProfile.classList.remove("popup_opened");
-}
-function handleOpenPopup(popUpAdd) {
-  popUpAdd.classList.add("popup_opened");
-}
-function handleClosePopup(popUpAdd) {
-  popUpAdd.classList.remove("popup_opened");
-}
 const renderCards = () => {
   const items = initialCards.map(getItems);
   sectionElements.append(...items);
